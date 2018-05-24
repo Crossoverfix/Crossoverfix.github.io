7e3d4d22dd7d200ff602af6d9432a25055c65e73
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2018-05-24 19:34:48 +0300

    v1.04
    after tabs and tabs.js

---
+++ syntelect/js/services__table.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+  var $tabs = $("div.services__table__control__point");
+  var $content = $("div.services__table__watch__point");
+  $tabs.click(function (eventObject) {
+    var $eventTab = eventObject.currentTarget;
+    clickTabs($eventTab);
+  })
+  function clickTabs ($eventTab) {
+    for (i = 0;i < $tabs.length;i++){
+      $tabs.eq(i).removeClass("active");
+      $content.eq(i).removeClass("active");
+    }
+    $($eventTab).addClass("active");
+    for (i = 0;i < $tabs.length;i++){
+      if ($tabs.eq(i).hasClass("active")){
+        $content.eq(i).addClass("active");
+      }
+    }
+  }
+})
