4bd22667bac37c044d32c3f983b37eb65ab543af
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2018-05-28 18:13:50 +0300

    v1.05
    contacts form

--- syntelect/js/script.js
+++ syntelect/js/script.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(function () {
+  var $contactForm = $("#contacts form");
+  var $formData = [];
+  var $formSubmint = $contactForm.find("button.button");
+  var $formFile = $contactForm.find("label");
+  $formData.name = $contactForm.find('input[name="name"]');
+  $formData.mail = $contactForm.find('input[name="email"]');
+  $formData.file = $contactForm.find('input[name="file"]');
+  $formData.subject = $contactForm.find('input[name="subject"]');
+  $formData.texta = $contactForm.find('input[name="textArea"]');
+  $formFile.click(function () {
+    fileSelect();
+  })
+  $contactForm.submit(function () {
+    submit()
+  })
+  function fileSelect () {
+    var $input = $formFile.find('input[type="file"]');
+    var $placeHold = $formFile.find("span:first");
+    $input.change(function () {
+      $placeHold.text($formData.file["0"].files["0"].name);
+    })
+  }
+  function submit () {
+    alert("name:" + $formData.name
+    + "\r Email:" + $formData.mail
+    + "\r file:" + $formData.file["0"].files["0"].name
+    + "\r subject:" + $formData.subject
+    + "\r textarea:" + $formData.texta);
+  }
+})
