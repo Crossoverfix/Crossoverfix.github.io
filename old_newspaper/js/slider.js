52b64b2d8dd40a5ccdb7b02eb9f360808cc27f0e
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2018-08-06 15:28:05 +0300

    v1.79

--- old_newspaper/js/slider.js
+++ old_newspaper/js/slider.js
@@ -3,44 +3,61 @@ $(document).ready(function () {
    var $sliderContent = $("#slider-all-content").find("img");
    var $sliderNavPrev = $("#slider-nav-bar").find("button:first-child");
    var $sliderNavNext = $("#slider-nav-bar").find("button:last-child");
-   var $sliderSmall_1 = $("div.content").find("div:nth-child(1)");
-   var $sliderSmall_2 = $("div.content").find("div:nth-child(2)");
-   var $sliderSmall_3 = $("div.content").find("div:nth-child(3)");
-   var $sliderSmall_4 = $("div.content").find("div:nth-child(4)");
-   var $activeSlides = $sliderContent.eq(0).clone();
-   $activeSlides.appendTo($watchArea);
+   var $sliderSmallSlide = $("#slider-nav-bar .content");
+   var $activSlide = $sliderContent.eq(0).clone().appendTo($watchArea);
+   watchSizeImg($activSlide);
    var $activeIndex = 0;
-   var $miniIndex_1 = 0;
-   var $miniIndex_2 = 0;
-   var $miniIndex_3 = 0;
-   var $miniIndex_4 = 0;
-   if ($activeSlides.outerWidth() < $watchArea.outerWidth()){
-       $activeSlides.css({'height':'auto','width':'100%'});
+   var $smallSlide = [];
+   for (i = 0;i < $sliderContent.length;i++){
+       $smallSlide[i] = $sliderContent.eq(i).clone().appendTo($sliderSmallSlide);
+       $smallSlide[i].wrap('<div></div>');
+       watchSizeImg($smallSlide[i]);
    }
-   if ($sliderContent.length < 2){
-      $sliderContent.eq(0).clone().appendTo($sliderSmall_1);
-      $sliderContent.eq(0).clone().appendTo($sliderSmall_2);
-      $sliderContent.eq(0).clone().appendTo($sliderSmall_3);
-      $sliderContent.eq(0).clone().appendTo($sliderSmall_4);
-
-   } else if ($sliderContent.length < 3){
-       $sliderContent.eq(0).clone().appendTo($sliderSmall_1);
-       $sliderContent.eq(1).clone().appendTo($sliderSmall_2);
-       $sliderContent.eq(0).clone().appendTo($sliderSmall_3);
-       $sliderContent.eq(1).clone().appendTo($sliderSmall_4);
-
-   } else if ($sliderContent.length < 4){
-       $sliderContent.eq(0).clone().appendTo($sliderSmall_1);
-       $sliderContent.eq(1).clone().appendTo($sliderSmall_2);
-       $sliderContent.eq(2).clone().appendTo($sliderSmall_3);
-       $sliderContent.eq(0).clone().appendTo($sliderSmall_4);
-   } else {
-       $sliderContent.eq(0).clone().appendTo($sliderSmall_1);
-       $sliderContent.eq(1).clone().appendTo($sliderSmall_2);
-       $sliderContent.eq(2).clone().appendTo($sliderSmall_3);
-       $sliderContent.eq(3).clone().appendTo($sliderSmall_4);
+   var $smalIndex = 0;
+   setInterval(autoPlay,6000);
+   function watchSizeImg($object) {
+       if($object.outerWidth() < $object.parent().outerWidth()){
+           $object.css({'width':'100%','height':'auto'});
+       } else {
+           $object.css({'width':'auto','height':'100%'});
+       }
    }
    function autoPlay() {
+       function clearLast($object){
+           $object.remove();
+       }
+       var $oldSlide;
+       if ($activeIndex < $sliderContent.length - 1){
+           $activeIndex++;
+           var $tempSlides = $sliderContent.eq($activeIndex).clone().appendTo($watchArea);
+           $oldSlide = $activSlide;
+           $activSlide = $tempSlides;
+           $oldSlide.animate({marginLeft:- $activSlide.outerWidth() + 1},1000);
+           var $anim = $.when($oldSlide.animate({marginLeft:- $activSlide.outerWidth() + 1},1000));
+           $anim.done(function () {
+               $oldSlide.remove();
+           })
+           $sliderSmallSlide.find("div:first-child").animate({marginLeft:- $sliderSmallSlide.find("div:first-child").outerWidth() - 5},1000);
+           var $smallAnim = $.when($sliderSmallSlide.find("div:first-child").animate({marginLeft:- $sliderSmallSlide.find("div:first-child").outerWidth() - 5},1000));
+           $smallAnim.done(function () {
+               $sliderSmallSlide.find("div:first-child").css({'margin-left':'5px'}).appendTo($sliderSmallSlide);
+           })
 
+       } else {
+           $activeIndex = 0;
+           var $tempSlides = $sliderContent.eq($activeIndex).clone().appendTo($watchArea);
+           $oldSlide = $activSlide;
+           $activSlide = $tempSlides;
+           $oldSlide.animate({marginLeft:- $activSlide.outerWidth()},1000);
+           var $anim = $.when($oldSlide.animate({marginLeft:- $activSlide.outerWidth()},1000));
+           $anim.done(function () {
+               $oldSlide.remove();
+           })
+           $sliderSmallSlide.find("div:first-child").animate({marginLeft:- $sliderSmallSlide.find("div:first-child").outerWidth() - 5},1000);
+           var $smallAnim = $.when($sliderSmallSlide.find("div:first-child").animate({marginLeft:- $sliderSmallSlide.find("div:first-child").outerWidth() - 5},1000));
+           $smallAnim.done(function () {
+               $sliderSmallSlide.find("div:first-child").css({'margin-left':'5px'}).appendTo($sliderSmallSlide);
+           })
+       }
    }
 });
