c092924fc69f379c05750643c0cc42f9712ae93f
Crossoverfix <39341735+Crossoverfix@users.noreply.github.com>
2018-09-16 16:26:00 +0300

    v3.15

--- pgubarev/js/script.js
+++ pgubarev/js/script.js
@@ -7,4 +7,27 @@ $(document).ready(function () {
             $("#nav-bar").css({'position':'absolute','top':'auto','bottom':'0'});
         }
     });
+    var $tabs = $(".btn-tabs");
+    var $tabsContent = $(".biography__content__point");
+    $tabs.on('click',function () {
+        var $indexTabs = $($tabs).index(this);
+        $tabs.removeClass('active');
+        $tabsContent.removeClass('active');
+        $tabs.eq($indexTabs).addClass('active');
+        $tabsContent.eq($indexTabs).addClass('active');
+    })
+    var $newsBlog = $(".news-blog__header h2");
+    $newsBlog.on('click',function () {
+        if($newsBlog.index(this) == 0){
+            $newsBlog.removeClass('active');
+            $newsBlog.eq(0).addClass('active');
+            $(".news-blog__body.news").css('display','flex');
+            $(".news-blog__body.blog").css('display','none');
+        } else if ($newsBlog.index(this) == 2){
+            $newsBlog.removeClass('active');
+            $newsBlog.eq(2).addClass('active');
+            $(".news-blog__body.news").css('display','none');
+            $(".news-blog__body.blog").css('display','flex');
+        }
+    })
 })
